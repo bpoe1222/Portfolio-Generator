93e049513f922bd3fb9a758726cb8725e14b1fea
Bailey Poe <baileypoe1222@gmail.com>
2021-09-17 16:45:09 -0500

    created html document from user input

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
+const fs = require('fs');
 const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, github] = profileDataArgs;
+const generatePage = require('./src/page-template.js');
 
-const printProfileData = (profileDataArr) => {
-    profileDataArr.forEach((profileItem) => {
-        console.log(profileItem)
-    });
-};
+// const printProfileData = (profileDataArr) => {
+//     profileDataArr.forEach((profileItem) => {
+//         console.log(profileItem)
+//     });
+// };
 
-printProfileData(profileDataArgs);
+// printProfileData(profileDataArgs);
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
