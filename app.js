3d05dcc85070aeb46bbe5dc09ce204986cee2030
Bailey Poe <baileypoe1222@gmail.com>
2021-09-17 19:36:01 -0500

    start of questions section

--- app.js
+++ app.js
@@ -3,14 +3,6 @@ const profileDataArgs = process.argv.slice(2, process.argv.length);
 const [name, github] = profileDataArgs;
 const generatePage = require('./src/page-template.js');
 
-// const printProfileData = (profileDataArr) => {
-//     profileDataArr.forEach((profileItem) => {
-//         console.log(profileItem)
-//     });
-// };
-
-// printProfileData(profileDataArgs);
-
 fs.writeFile('index.html', generatePage(name, github), err => {
     if (err) throw err;
 
